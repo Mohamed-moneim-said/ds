c91aaf6ec6d2769059d3e73cb4d4c7d0161da100
Mohamed-moneim-said <55868763+Mohamed-moneim-said@users.noreply.github.com>
2019-09-27 10:57:15 +0200

    Update system.s

--- darkstar/system.s
+++ darkstar/system.s
@@ -2,5 +2,6 @@ processor 6085
 
 .include "fr/system.fr" ;????
 .include "system/font.fr" ; for set font
+.include "fr/darkstar-prom.fr"
 
 ;;;==========================================================
